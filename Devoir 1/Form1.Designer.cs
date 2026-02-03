6f61ae6d1562454396ac32ac0744d5ff268636d9
Basnat ALBASHITI <Basnat.ALBASHITI@student.hel.be>
2026-02-03 10:57:21 +0100

    ajouter un label changer le font et color

--- Devoir 1/Form1.Designer.cs
+++ Devoir 1/Form1.Designer.cs
@@ -28,18 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.IndianRed;
+            label1.Location = new Point(52, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(41, 38);
+            label1.TabIndex = 0;
+            label1.Text = "l1";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "F1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
